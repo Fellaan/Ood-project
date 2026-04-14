9b1b25db72f769aa554a6a77f6363e836086a0c6
Fellaan <felix-lilja@hotmail.com>
2026-04-14 13:49:07 +0200

    Login menu class and some logic in main to be able to call it

---
+++ OodGood/app/src/main/java/g1/presentation/LoginMenu.java
@@ -0,0 +1,58 @@
+package g1.presentation;
+
+import java.util.Scanner;
+
+
+public class LoginMenu {
+    String username = "admin";
+    String password = "admin";
+    String usr;
+    String psw;
+
+    public void runMenu(){
+
+        Boolean correctLogin = false;
+
+        //Allt nedanför ersätts när vi ska skapa vidare med login funktionen
+
+        do {
+            System.out.print("Username: ");
+            usr = getChoice();
+            
+            System.out.print("Password: ");
+            psw = getChoice();
+            
+
+            if (usr.equals(username) && psw.equals(password) || usr.equals("user") && psw.equals("user") ){
+                correctLogin = true;   
+            }
+
+            else{
+                System.out.println("Login not correct, please try agian");
+            }
+        } while (!correctLogin);
+
+        System.out.println("Nice login bro");
+        if (usr.equals("admin")){
+            System.out.println("Damn admin is mogging");
+        }else{
+            System.err.println("Lil bro");
+        }
+
+
+
+    }
+
+
+
+
+  public String getChoice(){
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        return scanner.nextLine();
+    }
+
+
+
+}
