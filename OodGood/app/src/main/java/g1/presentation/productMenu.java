25d4a481c4c6b24992747b2c77fefe5e977ab6be
Leo <leo.engkvist_qvarnstrom0016@stud.hkr.se>
2026-04-15 09:10:25 +0200

    Made productMenu

---
+++ OodGood/app/src/main/java/g1/presentation/productMenu.java
@@ -0,0 +1,73 @@
+package g1.presentation;
+
+import java.util.Scanner;
+
+public class productMenu {
+    private Scanner scanner;
+
+    public productMenu() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        String choice;
+
+        printMenu();
+
+        do {
+            choice = getChoice();
+            switch (choice) {
+                case "1":  // Skapa ny produkt
+                    System.out.println("Creating new product...\n" );
+                    printMenu(); // Får upp menyn igen efter varje val för enklare navigering.
+                    break;
+                case "2":   // Ta bort produkt som specifieras
+                    System.out.println("Removing product...\n");
+                    printMenu();
+                    break;
+                case "3":   // Skriv ut lista på alla produkter som finns
+                    System.out.println("Listing all products...\n");
+                    printMenu();
+                    break;
+                case "4":   // Visar information på en produkt som specifieras
+                    System.out.println("Showing info of product X: ...\n");
+                    printMenu();
+                    break;
+                case "5":   // Räknar ut miljö påverkan av produkt som specifieras
+                    System.out.println("Calculating impact of product X ...\n");
+                    printMenu();
+                    break;
+                case "9":
+                    System.out.println("Buh bye!");
+                    break;
+            
+                default:
+                    System.err.println("Not a valid menu option, please try again!");
+                    
+            }
+        } while (!(choice.equals("9")));
+    }
+
+
+    private String getChoice() {        //Öppnar ny scanner, det skulle vi lösa på annat sätt?
+        System.out.print("Enter menu alternative: ");
+        String input = scanner.nextLine();
+        return input;
+    }
+
+
+    private void printMenu() {          // Meny layout, idéer på design välkomnas.
+        String menuText = """
+                 ---------------- Product Menu ----------------
+                | 1) Create new product
+                | 2) Remove an existing product
+                | 3) List all existing products
+                | 4) Show info of specific product
+                | 5) Calculate environmental impact of a product
+                | 9) Return to Main menu
+                 ----------------------------------------------""";
+
+        System.out.println(menuText);
+    }
+
+}
