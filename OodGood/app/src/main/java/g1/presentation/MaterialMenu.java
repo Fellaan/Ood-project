266e8e5223a8d5b84231236758dc0ad55cd1f49d
= <isak.larsson0032@stud.hkr.se>
2026-04-16 10:44:01 +0200

    Added connection to Material Application Service

--- OodGood/app/src/main/java/g1/presentation/MaterialMenu.java
+++ OodGood/app/src/main/java/g1/presentation/MaterialMenu.java
@@ -1,13 +1,16 @@
 package g1.presentation;
 
 import java.util.Scanner;
+import g1.application.MaterialApplicationService;
 
 public class MaterialMenu {
 
     Scanner scanner;
+    MaterialApplicationService mas;
 
-    public MaterialMenu(Scanner scanner){
+    public MaterialMenu(Scanner scanner, MaterialApplicationService mas){
         this.scanner = scanner;
+        this.mas = mas;
     }
 
     public void run(){
@@ -20,15 +23,19 @@ public class MaterialMenu {
             switch (choice) {
                 case "1":
                     System.out.println("Here you can create a Material");
+                    createMaterial();
                     break;
                 case "2":
                     System.out.println("Here you can remove a Material");
+                    removeMaterial();
                     break;
                 case "3":
                     System.out.println("Here you can list Materials");
+                    listMaterials();
                     break;
                 case "4":
                     System.out.println("Here you can see Material Info");
+                    showMaterialInfo();
                     break;
                 case "5":
                     System.out.println("Returning to the Main menu");
@@ -55,9 +62,34 @@ public class MaterialMenu {
                 | 2) Remove Material
                 | 3) List Materials
                 | 4) Show Material Info
-                | 5) Return to Main Menu
-                ----------------------------------------------"""
+                | 9) Return to Main Menu
+                -----------------------------------------------"""
             );
+    }
+
+    public void createMaterial(){
+        String name = getChoice();
+        String recyclingCategory = getChoice();
+        int environmentalImpact = Integer.parseInt(getChoice());
+        String message = mas.createMaterial(name, recyclingCategory, environmentalImpact);
+        System.out.println(message);
+    }
+
+    public void removeMaterial(){
+        String name = getChoice();
+        String message = mas.removeMaterial(name);
+        System.out.println(message);
+    }
+
+    public void listMaterials(){
+        String message = mas.listMaterials();
+        System.out.println(message);
+    }
+
+    public void showMaterialInfo(){
+        String name = getChoice();
+        String message = mas.showInfo(name);
+        System.out.println(message);
     }
 
 
