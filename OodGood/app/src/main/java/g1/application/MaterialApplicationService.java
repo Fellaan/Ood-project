c559723022d3d4d2b891fbb088e8ea19f4f07448
Fellaan <felix-lilja@hotmail.com>
2026-04-16 10:44:12 +0200

    Created methods in Material Application Service

--- OodGood/app/src/main/java/g1/application/MaterialApplicationService.java
+++ OodGood/app/src/main/java/g1/application/MaterialApplicationService.java
@@ -2,4 +2,43 @@ package g1.application;
 
 public class MaterialApplicationService {
 
+    Repository materialRepository;
+
+
+public MaterialApplicationService(Repository materialRepository){
+
+this.materialRepository = materialRepository;
+    
+}
+// Method will be finished later
+public String createMaterial(String name, String category, int EnvironmentalImpact){
+    
+
+
+    return "Created Material";
+}
+
+
+// Method will be finished later
+public String removeMaterial(String name){
+
+    return "Removed Material";
+}
+
+
+
+// Method will be finished later 
+public String listMaterials(){
+
+    return "List of materials";
+
+}
+// Method will be finished later 
+public String showInfo(String name){
+
+    return "Showing info";
+}
+
+
+
 }
