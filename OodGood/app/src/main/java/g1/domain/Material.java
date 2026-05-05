15b962192f377387ca0874453d35c75e6b404030
Fellaan <felix-lilja@hotmail.com>
2026-05-05 10:13:11 +0200

    Bugfix, changed environmental impact to emission factor

--- OodGood/app/src/main/java/g1/domain/Material.java
+++ OodGood/app/src/main/java/g1/domain/Material.java
@@ -6,12 +6,12 @@ public class Material implements Serializable {
 
     String name;
     String recyclingCategory;
-    int environmentalImpact;
+    double emissionFactor;
 
-    public Material(String name, String recyclingCategory, int environmentalImpact){
+    public Material(String name, String recyclingCategory, double emissionFactor){
         this.name = name;
         this.recyclingCategory = recyclingCategory;
-        this.environmentalImpact = environmentalImpact;
+        this.emissionFactor = emissionFactor;
 
     }
 
@@ -19,12 +19,12 @@ public class Material implements Serializable {
         return name;
     }
 
-    public String getrecyclingCategory(){
+    public String getRecyclingCategory(){
         return recyclingCategory;
     }
 
-    public int getenvironmentalImpact(){
-        return environmentalImpact;
+    public double getEmissionFactor(){
+        return emissionFactor;
     }
 
 }
