187214b032a548dd4b9d3777202f83a659d4f884
Mjaulus <paul_94@live.se.com>
2026-05-12 10:42:36 +0200

    added unitTest for SimpleSumStrategy

---
+++ OodGood/app/src/test/java/SimpleSumStrategyTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import  org.junit.jupiter.api.BeforeEach;
+
+import g1.domain.*;
+
+public class SimpleSumStrategyTest {
+ImpactCalculationStrategy strategy;
+        HashMap<Material, Double> materials;
+
+    @BeforeEach void setup(){ 
+        
+         strategy = new SimpleSumStrategy();
+
+          Material plastic = new Material(
+                "Plastic",
+                "Recycle",
+                5.0
+        );
+
+        Material glass = new Material(
+                "Glass",
+                "Recycle",
+                2.5);
+
+        materials = new HashMap<>();
+
+        materials.put(plastic, 2.0);
+        materials.put(glass, 4.0);
+
+    }
+
+    @Test
+    void shouldCalculateTotalImpactCorrectly() {
+        
+
+        // Act
+        double result = strategy.calculateImpact(materials, 10);
+
+        // Assert
+        assertEquals(20.0, result);
+    }
+
+
+    @Test
+    void shouldReturnZeroWhenNoMaterialsExist() {
+
+        HashMap<Material, Double> emptyMaterials = new HashMap<>();
+
+        // Act
+        double result = strategy.calculateImpact(emptyMaterials, 10);
+
+        // Assert
+        assertEquals(0.0, result);
+    }
+}
